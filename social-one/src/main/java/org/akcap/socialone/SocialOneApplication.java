7f6f4cb2c63b9ea680bbd6427d19dc3091c15bb6
akamarkotwal <akamarkotwal@gmail.com>
2020-05-13 10:00:49 +0530

    project structure

---
+++ social-one/src/main/java/org/akcap/socialone/SocialOneApplication.java
@@ -0,0 +1,13 @@
+package org.akcap.socialone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialOneApplication.class, args);
+	}
+
+}
---
+++ social-one/src/test/java/org/akcap/socialone/SocialOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akcap.socialone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
