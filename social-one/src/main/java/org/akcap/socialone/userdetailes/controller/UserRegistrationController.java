21170f09a0a65f70d70a74a5e819a079879c739c
akamarkotwal <akamarkotwal@gmail.com>
2020-05-16 11:12:11 +0530

    1.Handling Exception of json body 2.Resolved Bug of Duplicate Entry of user

--- social-one/src/main/java/org/akcap/socialone/userdetailes/controller/UserRegistrationController.java
+++ social-one/src/main/java/org/akcap/socialone/userdetailes/controller/UserRegistrationController.java
@@ -5,6 +5,8 @@ import org.akcap.socialone.entity.UserInfomation;
 import org.akcap.socialone.userdetailes.model.UserDTO;
 import org.akcap.socialone.userdetailes.service.UserRegisterServices;
 import org.akcap.socialone.util.SingaleResponceMessages;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,39 +18,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/socialone")
 public class UserRegistrationController {
 
+	private static final Logger LOGGER = (Logger) LogManager.getLogger(UserRegistrationController.class);
+
 	@Autowired
 	private UserRegisterServices userRegisterServices;
 
 	@PostMapping(value = "/register")
 	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
 		SingaleResponceMessages<?> responseMessage = new SingaleResponceMessages<>();
-		UserInfomation userInfomation = userRegisterServices.getEmail(user.getEmail());
-		if (userInfomation != null) {
-			responseMessage.setStatuscode(0);
-			responseMessage.setStatus("Failed");
-			responseMessage.setMessage("User Email Alredy Register With Different Account");
-			responseMessage.setData(null);
-			return ResponseEntity.ok(responseMessage);
-		}
+		if (user.getFname() != null && user.getLname() != null && user.getEmail() != null && user.getDob() != null
+				&& user.getGenderId() != null && user.getPassword() != null) {
 
-		UserInfomation infomation = userRegisterServices.saveUser(user);
+			UserInfomation userInfomation = userRegisterServices.getEmail(user.getEmail());
+			if (userInfomation != null) {
+				LOGGER.error("User alredy present in database");
 
-		if (infomation != null) {
-			UserResponse response = new UserResponse();
-			response.setUserID(infomation.getId());
-			response.setUserName(infomation.getUserName());
-			responseMessage.setStatuscode(1);
-			responseMessage.setStatus("Success");
-			responseMessage.setMessage("User Record Insert Successfully");
-			responseMessage.setData(response);
+				responseMessage.setStatuscode(0);
+				responseMessage.setStatus("Failed");
+				responseMessage.setMessage("User Email Alredy Register With Different Account");
+				responseMessage.setData(null);
+				return ResponseEntity.ok(responseMessage);
+			}
 
-			return ResponseEntity.ok(responseMessage);
+			UserInfomation infomation = userRegisterServices.saveUser(user);
+
+			if (infomation != null) {
+				UserResponse response = new UserResponse();
+				response.setUserID(infomation.getId());
+				response.setUserName(infomation.getUserName());
+				responseMessage.setStatuscode(1);
+				responseMessage.setStatus("Success");
+				responseMessage.setMessage("User Record Insert Successfully");
+				responseMessage.setData(response);
+
+				return ResponseEntity.ok(responseMessage);
+			} else {
+				responseMessage.setStatuscode(0);
+				responseMessage.setStatus("Failed");
+				responseMessage.setMessage("User Record Insert UnSuccessfully");
+				responseMessage.setData(null);
+				return ResponseEntity.ok(responseMessage);
+			}
+
 		} else {
+			LOGGER.error("Field is missing from body");
 			responseMessage.setStatuscode(0);
 			responseMessage.setStatus("Failed");
-			responseMessage.setMessage("User Record Insert UnSuccessfully");
+			responseMessage.setMessage("Please Provide All Details");
 			responseMessage.setData(null);
 			return ResponseEntity.ok(responseMessage);
+
 		}
 
 	}
